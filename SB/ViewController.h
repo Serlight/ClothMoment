1e577b4610dfa1dce2072f0cbc491851b07bf962
spring <spring@dev.healtho.com>
2014-10-23 20:20:46 +0800

    Initial Commit

---
+++ SB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SB
+//
+//  Created by serlight on 10/23/14.
+//  Copyright (c) 2014 soubu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SB/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SB
+//
+//  Created by serlight on 10/23/14.
+//  Copyright (c) 2014 soubu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
